6fe464f3afeae2e168a2349e9b1a183184eee8e4
Aanya09 <71958511+Aanya09@users.noreply.github.com>
2021-01-04 14:54:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro-35
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var  database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+ 
+}
+
+function draw(){
+  background("white");
+  
+   
+  
+}
+
+
